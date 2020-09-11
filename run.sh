50a97d974fd7f105c907a1fbdd33d8a5672db48f
Alan Hollis <me@alanhollis.com>
2020-09-11 15:18:41 +0100

    Fix run command

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /vault/secrets/babybuddy 
-/bin/bash -c python manage.py migrate --noinput && python manage.py createcachetable && gunicorn babybuddy.wsgi -b :8000 --log-level=info
+python manage.py migrate --noinput && python manage.py createcachetable && gunicorn babybuddy.wsgi -b :8000 --log-level=info
