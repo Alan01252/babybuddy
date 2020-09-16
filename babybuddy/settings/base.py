5382527dc84530fe56a65c7452620bba41bfd668
Alan Hollis <me@alanhollis.com>
2020-09-16 12:33:07 +0100

    Add region setting

--- babybuddy/settings/base.py
+++ babybuddy/settings/base.py
@@ -212,6 +212,8 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY') or None
 
 AWS_S3_SIGNATURE_VERSION = os.environ.get('AWS_S3_SIGNATURE_VERSION') or None
 
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME') or None
+
 if AWS_STORAGE_BUCKET_NAME:
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     THUMBNAIL_DEFAULT_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
